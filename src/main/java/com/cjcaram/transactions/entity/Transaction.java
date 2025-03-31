97c65312469ff9c9765dbd0a1b56992cd2dfdb06
Cristian Caram <cristian.caram@kodaris.com>
2025-03-31 09:34:01 -0300

    add entities - In Progress

---
+++ src/main/java/com/cjcaram/transactions/entity/Transaction.java
@@ -0,0 +1,40 @@
+package com.cjcaram.transactions.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long accountId;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+}
---
+++ src/main/java/com/cjcaram/transactions/model/TransactionType.java
@@ -0,0 +1,6 @@
+package com.cjcaram.transactions.model;
+
+public enum TransactionType {
+    DEPOSIT, // DEPOSITO
+    WITHDRAW // RETIRO
+}
